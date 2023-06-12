1050020e6ab02982847d05aa354eb21025e28a04
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-12 10:35:02 +0300

    added knexfile config

---
+++ backend/src/config/knexfile.js
@@ -0,0 +1,61 @@
+import { database, env } from "./index.js";
+
+export default {
+
+  development: {
+    client: "mssql",
+    connection: {
+      server: database.host,
+      user: database.user,
+      password: database.password,
+      database: `${database.database}_${env}`,
+      options: {
+        port: database.port,
+        trustServerCertificate: true
+      }
+    }
+  },
+
+  staging: {
+    client: "mssql",
+    connection: {
+      server: database.host,
+      user: database.user,
+      password: database.password,
+      database: `${database.database}_${env}`,
+      options: {
+        port: database.port,
+        trustServerCertificate: true
+      }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "mssql",
+    connection: {
+      server: database.host,
+      user: database.user,
+      password: database.password,
+      database: `${database.database}_${env}`,
+      options: {
+        port: database.port,
+        trustServerCertificate: true
+      }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
