ce5bef1a7eb7795e28619484c32847b319518f2a
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-13 14:22:32 +0300

    importing home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,12 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import logo from './assets/react.svg'
+import "./App.css";
+import Home from "./pages/Home/index.jsx";
 
 function App() {
-  const [message, setMessage] = useState();
-  useEffect(() => {
-    fetch("/api/")
-      .then(res => res.json())
-      .then(res => setMessage(res.message))
-      .catch(console.error);
-  }, [setMessage]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message || "Loading..."}</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload to see stuff
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
 
-export default App
+export default App;
