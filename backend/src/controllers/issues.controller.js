b4fb68583efb99302a1d9f294faed99c795bbe74
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-13 05:24:07 +0300

    skipping creation of complainant on each request

--- backend/src/controllers/issues.controller.js
+++ backend/src/controllers/issues.controller.js
@@ -1,15 +1,25 @@
+import { fetchByPhoneNumber } from "../models/complainants.model.js";
 import db from "./../database.js";
 
 export async function create(req, res) {
   const phoneNumber = req.body.phone_number;
   const { name, email, subject, description, category_id } = req.body;
 
-  let complainantId = await db("complainants")
-    .insert({ name, email, phone_number: phoneNumber })
-    .returning("id");
+
+  let complainant = await fetchByPhoneNumber(phoneNumber);
+  let complainantId;
+  if (complainant.hasOwnProperty("id")) {
+    complainantId = complainant["id"];
+  } else {
+    let insertID = await db("complainants")
+      .insert({ name, email, phone_number: phoneNumber })
+      .returning("id");
+    complainantId = insertID[0];
+  }
+
 
   let issueID = await db
-    .insert({ title: subject, description, complainant_id: complainantId[0], category_id, status_id: 1 })
+    .insert({ title: subject, description, complainant_id: complainantId, category_id, status_id: 1 })
     .into("issues")
     .returning("id");
 
