0a383c642fda72533e0ab63fa375789f2e793428
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-13 03:49:30 +0300

    creating a new issue and saving in DB

--- backend/src/controllers/issues.controller.js
+++ backend/src/controllers/issues.controller.js
@@ -1,3 +1,17 @@
+import db from "./../database.js";
+
 export async function create(req, res) {
-  return res.json({ "message": "create a new issue" });
+  const phoneNumber = req.body.phone_number;
+  const { name, email, subject, description, category_id } = req.body;
+
+  let complainantId = await db("complainants")
+    .insert({ name, email, phone_number: phoneNumber })
+    .returning("id");
+
+  let issueID = await db
+    .insert({ title: subject, description, complainant_id: complainantId[0], category_id, status_id: 1 })
+    .into("issues")
+    .returning("id");
+
+  return res.status(201).json({ "message": `ticket created successfully with id ${issueID[0]}` });
 }
