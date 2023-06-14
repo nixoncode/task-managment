5879b03538a76550cffa77da7592a75babd05dc6
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-14 20:05:52 +0300

    added jwt secret

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,17 +1,17 @@
 import fs from "fs";
 
-
 const readFileSync = filename => fs.readFileSync(filename).toString("utf8");
-
 
 export const database = {
   host: process.env.DATABASE_HOST || "localhost",
   port: parseInt(process.env.DATABASE_PORT),
   database: process.env.DATABASE_DB,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD
+  password: process.env.DATABASE_PASSWORD,
 };
 
 export const port = process.env.PORT || 8080;
 
-export const env = process.env.NODE_ENV || 'development';
+export const env = process.env.NODE_ENV || "development";
+
+export const jwtSecret = process.env.JWT_SECRET;
