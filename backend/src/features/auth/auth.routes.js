e5e12e92faeaffa61613861006a95162ab976d7b
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-14 15:14:01 +0300

    validating request body

--- backend/src/features/auth/auth.routes.js
+++ backend/src/features/auth/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { login, register } from "./auth.handlers.js";
+import { registerValidationRules } from "./auth.validator.js";
+import validator from "../../middleware/validator.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", registerValidationRules(), validator, register);
 router.post("/login", login);
 
 export default router;
