8402ab99b0712af3a66f25280818f05fc2203a17
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-14 16:34:11 +0300

    removed unused users

--- backend/src/features/auth/auth.handlers.js
+++ backend/src/features/auth/auth.handlers.js
@@ -1,6 +1,20 @@
+import { createUser } from "./auth.model.js";
+
 export function login(req, res) {
   res.json({ message: "todo login" });
 }
-export function register(req, res) {
-  res.json({ message: "register page" });
+export async function register(req, res) {
+  const { name, email, phone, password } = req.body;
+
+  let result = await createUser(name, email, phone, password);
+
+  if (!result) {
+    return res
+      .status(400)
+      .json({ message: "something went wrong, try again in a little bit" });
+  }
+
+  return res.status(201).json({
+    message: `user registered successfully with id ${result}`,
+  });
 }
