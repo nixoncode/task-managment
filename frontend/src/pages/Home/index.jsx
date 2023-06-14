6b096e1610beab54b42d135e728dbc67196e2dc8
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-14 14:44:42 +0300

    submitting an issue

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -7,38 +7,93 @@ import { useState } from "react";
 import isMobilePhone from "validator/es/lib/isMobilePhone";
 
 export default function Home() {
-  const [mobileNumber, setMobileNumber] = useState({
-    value: "",
-    hasError: false,
-    errorMsg: "Invalid mobile number",
-  });
+  const [mobileNumber, setMobileNumber] = useState({ value: "", error: "" });
   const [isLoading, setIsLoading] = useState(false);
   const [showRestOfTheForm, setShowRestOfTheForm] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [description, setDescription] = useState("");
+  const [name, setName] = useState({ value: "", error: "" });
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [subject, setSubject] = useState({ value: "", error: "" });
+  const [description, setDescription] = useState({ value: "", error: "" });
 
   function continueMobile() {
     if (!isMobilePhone(mobileNumber.value, "en-KE")) {
-      setMobileNumber({ ...mobileNumber, hasError: true });
+      setMobileNumber({ ...mobileNumber, error: "Invalid mobile number" });
       return;
     }
-    setMobileNumber({ ...mobileNumber, hasError: false });
+    setMobileNumber({ ...mobileNumber, error: "" });
     setIsLoading(true);
 
     fetch(`/api/complainants/${mobileNumber.value}`)
       .then(res => res.json())
       .then(res => {
         // initialize form with data from server
-        setName(res.complainant.name);
-        setEmail(res.complainant.email);
+        setName({ ...name, value: res.complainant.name });
+        setEmail({ ...email, value: res.complainant.email });
       })
       .catch(console.error)
       .finally(() => {
         setIsLoading(false);
         setShowRestOfTheForm(true);
       });
+  }
+
+  function handleValidationErrors(errors) {
+    if (errors.hasOwnProperty("phone_number")) {
+      setMobileNumber({ ...mobileNumber, error: errors.phone_number });
+    }
+    if (errors.hasOwnProperty("name")) {
+      setName({ ...name, error: errors.name });
+    }
+
+    if (errors.hasOwnProperty("email")) {
+      setEmail({ ...email, error: errors.email });
+    }
+    if (errors.hasOwnProperty("subject")) {
+      setSubject({ ...subject, error: errors.subject });
+    }
+    if (errors.hasOwnProperty("description")) {
+      setDescription({ ...subject, error: errors.description });
+    }
+  }
+
+  function resetState() {
+    setMobileNumber({ value: "", error: "" });
+    setName({ value: "", error: "" });
+    setEmail({ value: "", error: "" });
+    setSubject({ value: "", error: "" });
+    setDescription({ value: "", error: "" });
+  }
+
+  async function handleSubmitIssue() {
+    try {
+      const response = await fetch("/api/issues", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name.value,
+          phone_number: mobileNumber.value,
+          email: email.value,
+          subject: subject.value,
+          description: description.value,
+          category_id: 5,
+        }),
+      });
+      let data = await response.json();
+
+      if (response.status === 422) {
+        const { message, errors } = data;
+        console.log(message);
+        handleValidationErrors(errors);
+        return;
+      }
+
+      if (response.status == 201) {
+        alert("submitted successfully");
+        resetState();
+      }
+    } catch (error) {
+      console.log(error, "error");
+    }
   }
 
   return (
@@ -62,7 +117,7 @@ export default function Home() {
                   type="tel"
                   placeholder="Enter phone number"
                   className={`input input-bordered join-item flex-1 ${
-                    mobileNumber.hasError ? "input-error" : "input-primary"
+                    mobileNumber.error ? "input-error" : "input-primary"
                   }`}
                   onChange={event =>
                     setMobileNumber({
@@ -86,11 +141,11 @@ export default function Home() {
                   </button>
                 )}
               </div>
-              {mobileNumber.hasError ? (
+              {mobileNumber.error && (
                 <span className="text-sm text-error flex mt-1 ml-1">
-                  {mobileNumber.errorMsg}
+                  {mobileNumber.error}
                 </span>
-              ) : null}
+              )}
             </div>
             <Transition
               show={showRestOfTheForm}
@@ -101,45 +156,79 @@ export default function Home() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="form flex py-2">
+              <div className="py-2">
                 <input
                   type="text"
                   placeholder="What is your name?"
-                  className="input input-bordered input-primary join-item flex-1"
-                  value={name}
-                  onChange={event => setName(event.target.value)}
+                  className={`input input-bordered ${
+                    name.error ? "input-error" : "input-primary"
+                  } w-full`}
+                  value={name.value}
+                  onChange={event =>
+                    setName({ ...name, value: event.target.value })
+                  }
                 />
+                {name.error && (
+                  <p className="text-sm text-error text-start px-2">
+                    {name.error}
+                  </p>
+                )}
               </div>
-              <div className="form flex py-2">
+              <div className="py-2">
                 <input
                   type="email"
                   placeholder="Enter your email address"
-                  className="input input-bordered input-primary join-item flex-1"
-                  value={email}
-                  onChange={event => setEmail(event.target.value)}
+                  className={`input input-bordered ${
+                    email.error ? "input-error" : "input-primary"
+                  } w-full`}
+                  value={email.value}
+                  onChange={e => setEmail({ ...email, value: e.target.value })}
                 />
+                {email.error && (
+                  <p className="text-sm text-error text-start px-2">
+                    {email.error}
+                  </p>
+                )}
               </div>
-              <div className="form flex py-2">
+              <div className="py-2">
                 <input
                   type="text"
                   placeholder="Subject"
-                  className="input input-bordered input-primary join-item flex-1"
-                  value={subject}
-                  onChange={event => setSubject(event.target.value)}
+                  className={`input input-bordered ${
+                    subject.error ? "input-error" : "input-primary"
+                  } w-full`}
+                  value={subject.value}
+                  onChange={e =>
+                    setSubject({ ...subject, value: e.target.value })
+                  }
                 />
+                {subject.error && (
+                  <p className="text-sm text-error text-start px-2">
+                    {subject.error}
+                  </p>
+                )}
               </div>
-              <div className="form flex py-2">
+              <div className="py-2">
                 <textarea
-                  className="textarea textarea-primary flex-1"
+                  className={`textarea ${
+                    description.error ? "textarea-error" : "textarea-primary"
+                  } w-full`}
                   placeholder="Enter Description"
-                  value={description}
+                  value={description.value}
                   rows={5}
-                  onChange={event => setDescription(event.target.value)}
+                  onChange={e =>
+                    setDescription({ ...description, value: e.target.value })
+                  }
                 ></textarea>
+                {description.error && (
+                  <p className="text-sm text-error text-start px-2">
+                    {description.error}
+                  </p>
+                )}
               </div>
               <button
                 className="btn btn-primary join-item w-full mt-2"
-                onClick={continueMobile}
+                onClick={handleSubmitIssue}
                 disabled={isLoading}
               >
                 Submit Issue
