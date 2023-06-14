e65c894e91eb63c11a961e824633afef6a4256ca
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-14 20:05:03 +0300

    added signJwt

--- backend/src/features/auth/auth.service.js
+++ backend/src/features/auth/auth.service.js
@@ -1,8 +1,22 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { jwtSecret } from "../../config/index.js";
+
 export function hashPassword(password) {
   return bcrypt.hashSync(password);
 }
 
 export function comparePassword(password, passwordHash) {
-  return bcrypt.compareSync(password, hashPassword);
+  return bcrypt.compareSync(password, passwordHash);
+}
+
+export function signJwt(payload) {
+  return jwt.sign(
+    {
+      ...payload,
+      iat: Math.floor(Date.now() / 1000) - 30, // 30 secs ago
+    },
+    jwtSecret,
+    { expiresIn: "1h" },
+  );
 }
