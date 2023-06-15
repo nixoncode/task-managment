e7fdf9ce52fbac80a3beacb3c0cd47d03f149f83
Nixon Kosgei <nkosgey6@gmail.com>
2023-06-15 10:52:39 +0300

    added register page

---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import AuthLayout from "../../layouts/AuthLayout.jsx";
+import { Input } from "../../components/Input.jsx";
+import { Link } from "react-router-dom";
+
+export default function Register() {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const { name, email, phone, password } = credentials;
+
+  const handleChange = e => {
+    const { id, value } = e.target;
+    setCredentials(prev => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  return (
+    <AuthLayout>
+      <h1 className="w-full text-4xl text-center">Create an account</h1>
+      <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
+        <Input
+          label="Name"
+          id="name"
+          type="text"
+          placeholder="Enter your full name"
+          error=""
+          value={name}
+          onChange={handleChange}
+        />
+
+        <Input
+          label="Email address"
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          error=""
+          value={email}
+          onChange={handleChange}
+        />
+        <Input
+          label="Mobile number"
+          id="phone"
+          type="tel"
+          placeholder="Enter your phone number"
+          error=""
+          value={phone}
+          onChange={handleChange}
+        />
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          placeholder="Your password"
+          value={password}
+          onChange={handleChange}
+        />
+        <a className="btn btn-primary w-full text-xl">Create Account</a>
+        <p className="text-sm text-center text-gray-400">
+          Have an account?
+          <Link
+            to="/auth/login"
+            className="font-semibold text-primary focus:text-primary focus:outline-none focus:underline"
+          >
+            &nbsp;Login
+          </Link>
+        </p>
+      </div>
+    </AuthLayout>
+  );
+}
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./pages/Home/index.jsx";
 import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./pages/Error.jsx";
 
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/auth/login",
     element: <Login />,
+  },
+  {
+    path: "/auth/register",
+    element: <Register />,
   },
 ]);
 
